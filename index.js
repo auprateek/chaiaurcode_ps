e6ae526729baec503c26019c18658e8ff2cf782b
auprateek <prateeksharma2612@gmail.com>
2024-05-14 05:01:55 +0530

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+ 
+require('dotenv').config()
+
+const port = process.env.PORT
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World from pRateek')
+})
+
+app.get('/twitter', (req, res) =>{
+    res.send('<h2>youtube.com</h2>')
+  })
+app.listen(port, (req,res)=>{
+    console.log(`Port active ${port}`)
+})
